4239b776ca6d4ecd78632b8f274e714489774c8a
williamholtz <Willholtz2001@gmail.com>
2022-12-07 17:05:18 -0700

    Added path info for test file

--- examples/test.R
+++ examples/test.R
@@ -2,12 +2,14 @@ library(devtools)
 install_github("W-Holtz/R4scSHARP")
 library(R4scSHARP)
 
-# input vars
-data_path <- "splat_0.8/query_counts.csv.gz"
-out_path <- "output"
-marker_path <- "splat_0.8/markers.txt"
-ref_path <- "splat_0.8/ref_counts.csv.gz"
-ref_label_path <- "splat_0.8/ref_labels.csv"
+# input vars:
+# paths should be relative to your working directory
+# (call getwd() to get this information).
+data_path <- "examples/splat_0.8/query_counts.csv.gz"
+out_path <- "examples/output"
+marker_path <- "examples/splat_0.8/markers.txt"
+ref_path <- "examples/splat_0.8/ref_counts.csv.gz"
+ref_label_path <- "examples/splat_0.8/ref_labels.csv"
 
-output <- run_r4scsharp(data_path, out_path, marker_path, 
+output <- run_r4scsharp(data_path, out_path, marker_path,
     ref_path, ref_label_path)
